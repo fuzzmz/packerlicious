d1d83413aeb826b2c4d76972cc6325f798add1ce
Serban Constantin <serban.constantin@sparkware.ro>
2017-10-18 13:17:21 +0300

    add debug messages to find out where build hangs

--- tools/get-packer.ps1
+++ tools/get-packer.ps1
@@ -1,8 +1,11 @@
 $latestVersion = [string](((Invoke-RestMethod -Uri https://releases.hashicorp.com/packer/) -split "\n" | select-string -pattern "(\d+\.){2,3}\d+" | foreach {$_.matches} | select -expandproperty value) | %{[System.Version]$_} | sort | select -last 1)
+Write-Host "Latest version is $latestVersion"
 $url = "https://releases.hashicorp.com/packer/$latestVersion/packer_$latestVersion_windows_amd64.zip"
 $output = "$PSScriptRoot\packer.zip"
 
+Write-Host "Going to start download"
 (New-Object System.Net.WebClient).DownloadFile($url, $output)
+Write-Host "Finished download. Going to unzip"
 $extractPath = (get-item $PSScriptRoot).parent.FullName
 $shell = new-object -com shell.application
 $zip = $shell.NameSpace($output)
