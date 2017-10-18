78dff809fd640ba6d1b49534e2699d450312f419
Serban Constantin <serban.constantin@sparkware.ro>
2017-10-18 17:04:07 +0300

    fix variable

--- tools/get-packer.ps1
+++ tools/get-packer.ps1
@@ -1,7 +1,7 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $latestVersion = [string](((Invoke-RestMethod -Uri https://releases.hashicorp.com/packer/) -split "\n" | select-string -pattern "(\d+\.){2,3}\d+" | foreach {$_.matches} | select -expandproperty value) | %{[System.Version]$_} | sort | select -last 1)
 Write-Host "Latest version is $latestVersion"
-$url = "https://releases.hashicorp.com/packer/$latestVersion/packer_$latestVersion_windows_amd64.zip"
+$url = "https://releases.hashicorp.com/packer/$latestVersion/packer_${latestVersion}_windows_amd64.zip"
 Write-Host "Download link is $url"
 $output = "$PSScriptRoot\packer.zip"
 
